f9de74a36ba0961c481ca61572de6f2dc889c243
ch0pex <100431481@alumnos.uc3m.es>
2024-11-02 00:14:10 +0100

    Draft 1

--- Source/Editor/editor.hpp
+++ Source/Editor/editor.hpp
@@ -1,6 +1,6 @@
 /************************************************************************
  * Copyright (c) 2024 Alvaro Cabrera Barrio
- * This code is licensed under MIT license (see LICENSE.txt for details) 
+ * This code is licensed under MIT license (see LICENSE.txt for details)
  ************************************************************************/
 /**
  * @file editor.hpp
@@ -13,17 +13,16 @@
 
 #pragma once
 
-#include "utils.hpp"
-#include "IMGUI/imgui.h"
-#include "IMGUI/backends/imgui_impl_win32.h"
 #include "IMGUI/backends/imgui_impl_dx12.h"
+#include "IMGUI/backends/imgui_impl_win32.h"
+#include "IMGUI/imgui.h"
+#include "utils.hpp"
 
 #include "Editor/viewport/console.hpp"
 #include "Editor/viewport/dock_space.hpp"
 #include "Editor/viewport/entity_properties.hpp"
 #include "Editor/viewport/file_explorer.hpp"
 #include "Editor/viewport/scene_graph.hpp"
-#include "Project/project.hpp"
 #include "render/viewport.hpp"
 
 namespace reveal3d::ui {
@@ -31,7 +30,7 @@ namespace reveal3d::ui {
 template<graphics::HRI Gfx, window::Manager<Gfx> Window>
 class Editor {
 public:
-    Editor(Project& project, window::Config &window_info);
+    Editor(window::Info& window_info);
     ~Editor();
     void init();
     void run();
@@ -43,25 +42,23 @@ private:
 
     DockSpace dock_space_;
     EntityProperties entity_properties_;
-//    WorldProperties world_properties_;
+    //    WorldProperties world_properties_;
     SceneGraph scene_graph_;
     Console console_;
     FileExplorer explorer_;
     render::Viewport<Gfx, Window> viewport_;
-    Project& project_;
 };
 
 template<graphics::HRI Gfx, window::Manager<Gfx> Window>
-Editor<Gfx, Window>::Editor(Project& project, window::Config &window_info) :
-    viewport_(window_info), project_(project), explorer_(project.rootPath())
-{
+Editor<Gfx, Window>::Editor(window::Info& window_info) : explorer_("C:"), viewport_(window_info) {
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
-    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable; // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable; // Enable Multi-Viewport / Platform Windows
     io.ConfigViewportsNoAutoMerge = true;
     io.ConfigViewportsNoTaskBarIcon = true;
     io.WantCaptureKeyboard = false;
@@ -76,30 +73,29 @@ Editor<Gfx, Window>::Editor(Project& project, window::Config &window_info) :
 }
 
 template<>
-void Editor<graphics::Dx12, window::Win32>::init() {
-    auto &graphics = viewport_.renderer.graphics();
+inline void Editor<graphics::Dx12, window::Win32>::init() {
+    auto& graphics = viewport_.renderer.graphics();
     core::scene.init();
     viewport_.window.create(viewport_.renderer);
     viewport_.renderer.init(viewport_.window.getHandle());
 
     ImGui_ImplWin32_Init(viewport_.window.getHandle().hwnd);
-    ImGui_ImplDX12_Init(graphics.device(), 3,
-                        DXGI_FORMAT_R8G8B8A8_UNORM, graphics.heaps().srv.get(),
+    ImGui_ImplDX12_Init(graphics.device(), 3, DXGI_FORMAT_R8G8B8A8_UNORM, graphics.heaps().srv.get(),
                         graphics.heaps().srv.cpuStart(), graphics.heaps().srv.gpuStart());
 }
 
 template<>
-void Editor<graphics::OpenGL, window::Glfw>::init() {
-    //TODO
+inline void Editor<graphics::OpenGL, window::Glfw>::init() {
+    // TODO
 }
 
 template<graphics::HRI Gfx, window::Manager<Gfx> Window>
 void Editor<Gfx, Window>::run() {
     viewport_.window.show();
-    logger(logDEBUG) << "Initialized";
+    logger(LogDebug) << "Initialized";
 
     viewport_.timer.reset();
-    while(!viewport_.window.shouldClose()) {
+    while (!viewport_.window.shouldClose()) {
         viewport_.timer.tick();
         viewport_.window.update(viewport_.renderer);
         draw();
@@ -112,12 +108,11 @@ void Editor<Gfx, Window>::run() {
 template<graphics::HRI Gfx, window::Manager<Gfx> Window>
 void Editor<Gfx, Window>::benchMark(u32 seconds) {
     viewport_.window.show();
-    logger(logDEBUG) << "Initialized";
+    logger(LogDebug) << "Initialized";
 
     viewport_.timer.reset();
-    while(!viewport_.window.shouldClose()) {
-        [[unlikely]] if (seconds < viewport_.time().totalTime()) break;
-        
+    while (!viewport_.window.shouldClose()) {
+        [[unlikely]] if (seconds < viewport_.time().totalTime()) { break; }
         viewport_.timer.tick();
         viewport_.window.update(viewport_.renderer);
         draw();
@@ -126,7 +121,7 @@ void Editor<Gfx, Window>::benchMark(u32 seconds) {
         viewport_.renderer.render();
     }
 
-    logger(logDEBUG) << viewport_.time().meanFps() << "\n";
+    logger(LogDebug) << viewport_.time().meanFps() << "\n";
 }
 
 
@@ -136,18 +131,19 @@ void Editor<Gfx, Window>::terminate() {
 }
 
 template<>
-Editor<graphics::Dx12, window::Win32>::~Editor() {
+inline Editor<graphics::Dx12, window::Win32>::~Editor() {
     ImGui_ImplWin32_Shutdown();
     ImGui_ImplDX12_Shutdown();
     ImGui::DestroyContext();
 }
 
 template<>
-void Editor<graphics::Dx12, window::Win32>::draw() {
+inline void Editor<graphics::Dx12, window::Win32>::draw() {
     ImGui_ImplDX12_NewFrame();
     ImGui_ImplWin32_NewFrame();
     ImGui::NewFrame();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
     io.DeltaTime = viewport_.time().deltaTime();
 
     dock_space_.draw();
@@ -161,4 +157,4 @@ void Editor<graphics::Dx12, window::Win32>::draw() {
     ImGui::Render();
 }
 
-}
+} // namespace reveal3d::ui
